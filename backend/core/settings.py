e527d8b21edb8eac7994051839ad585ee8c2cfc8
Edwin Niwaha <ed107107@gmail.com>
2024-07-07 15:40:39 +0300

    add notes app

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -55,7 +55,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "apis.api",
+    "apis.notes",
     "apis.authentication",
     "corsheaders",
 ]
